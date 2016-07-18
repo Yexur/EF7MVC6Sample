1f1e0a42385332bc7e36f35b77893f5f6bd8d662
Yexur <shimojax@gmail.com>
2016-07-18 11:23:46 -0500

    Added View and Create to parent controller
    Added View and Create to parent controller

---
+++ GettingStarted/src/GettingStarted/Controllers/ParentController.cs
@@ -0,0 +1,43 @@
+﻿using GettingStarted.Logic;
+using GettingStarted.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GettingStarted.Controllers
+{
+    public class ParentController : Controller
+    {
+        private readonly IParentLogic _parentlogic;
+
+        public ParentController(IParentLogic parentLogic)
+        {
+            _parentlogic = parentLogic;
+        }
+
+        //GET
+        public IActionResult Index()
+        {
+            return View(_parentlogic.GetList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Parent parent)
+        {
+            if (ModelState.IsValid)
+            {
+                _parentlogic.Save(parent);
+                return RedirectToAction("Index");
+            }
+            return View(parent);           
+        }
+    }
+}
